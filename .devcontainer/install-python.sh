47e9f4272f2ea6ea63767a2a1452136c50814873
NeedToSetUsername <NeedToSetEmail@users.noreply.github.com>
2025-06-24 06:37:31 +0000

    feat: update devcontainer to use pre-built image.
    This is much faster for those first trying to install the devcontainer.

--- .devcontainer/install-python.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-apt-get update
-apt-get -y install python3 python3-ipykernel
-
-export UV_INSTALL_DIR="/usr/local/bin"
-curl -LsSf https://astral.sh/uv/install.sh | /bin/bash
-
-uv self update
